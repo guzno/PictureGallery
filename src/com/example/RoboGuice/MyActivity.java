0a952adc6a6ebe40f41aac0f89318477333bc19d
Drmiller <drmiller21@hotmail.com>
2013-02-27 23:42:46 +0100

    adding boilerplate

---
+++ src/com/example/RoboGuice/MyActivity.java
@@ -0,0 +1,49 @@
+package com.example.RoboGuice;
+
+import android.graphics.drawable.Drawable;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.inject.Inject;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectResource;
+import roboguice.inject.InjectView;
+
+public class MyActivity extends RoboActivity {
+    @InjectView(R.id.name)
+    TextView name;
+
+    @InjectView(R.id.thumbnail)
+    ImageView thumbnail;
+
+    @InjectResource(R.drawable.icon)
+    Drawable icon;
+
+    @InjectResource(R.string.app_name)
+    String myName;
+
+    @Inject
+    LocationManager loc;
+
+    @Inject
+    LayoutInflater inflater;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        name.setText("Hello, " + myName);
+    }
+
+    public void lol() {
+        if (loc == null)
+            loc = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        for (String provider : loc.getAllProviders()) {
+            Log.e("TAG", provider);
+        }
+        return;
+    }
+}
