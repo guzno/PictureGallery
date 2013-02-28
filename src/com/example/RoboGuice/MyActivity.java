99921e15d92a5faa5f3837860897c462ddb9155c
Drmiller <drmiller21@hotmail.com>
2013-02-28 01:42:22 +0100

    lol

--- src/com/example/RoboGuice/MyActivity.java
+++ src/com/example/RoboGuice/MyActivity.java
@@ -3,16 +3,13 @@ package com.example.RoboGuice;
 import android.graphics.drawable.Drawable;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.inject.Inject;
-import roboguice.RoboGuice;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
-import roboguice.inject.RoboInjector;
 
 public class MyActivity extends RoboActivity {
     @InjectView(R.id.name)
@@ -34,21 +31,10 @@ public class MyActivity extends RoboActivity {
     LayoutInflater inflater;
 
     public void onCreate(Bundle savedInstanceState) {
-        RoboInjector injector = RoboGuice.getInjector(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         name.setText("Hello, " + myName);
+        thumbnail.setImageDrawable(icon);
 
-        Log.d("TAG", "lol");
-    }
-
-    public void lol() {
-        if (loc == null)
-            loc = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        for (String provider : loc.getAllProviders()) {
-            Log.e("TAG", provider);
-        }
-        return;
     }
 }
