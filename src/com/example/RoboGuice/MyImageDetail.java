6abebfd305f5c25e65517174c2b31c47aa28905a
Gustav Nordlund <nordlund.gustav@gmail.com>
2013-03-01 16:42:05 +0100

    workeh

--- src/com/example/RoboGuice/MyImageDetail.java
+++ src/com/example/RoboGuice/MyImageDetail.java
@@ -1,12 +1,9 @@
 package com.example.RoboGuice;
 
-import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
-import roboguice.activity.RoboActivity;
 import roboguice.activity.RoboFragmentActivity;
-import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
 
 /**
@@ -16,18 +13,18 @@ import roboguice.inject.InjectView;
  * Time: 01:44
  * To change this template use File | Settings | File Templates.
  */
-public class GalleryActivity extends RoboFragmentActivity {
+public class MyImageDetail extends RoboFragmentActivity {
 
     @InjectView(R.id.image)
     ImageView image;
 
-    Drawable icon;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.gallery);
-        icon = getResources().getDrawable(getIntent().getIntExtra("img", 0));
-        //image.setImageDrawable(icon);
+
+        Drawable drawable = getResources().getDrawable(getIntent().getIntExtra(MyGallery.EXTRA_IMAGE_ID, 0));
+        image.setImageDrawable(drawable);
     }
 }
